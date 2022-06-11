c21de35821deb2e66928bb8ab782e48dc9677f3d
Brian Lee <brianlee1597@gmail.com>
2022-06-12 03:09:42 +0900

    carousel title addition

--- work/index.js
+++ work/index.js
@@ -21,3 +21,18 @@ document.addEventListener('mouseup', () => {
   cursor.classList.remove('click')
   cursorinner.classList.remove('cursorinnerhover')
 });
+
+const cards = new Array(5).fill(0).map((_, i) => document.getElementById(`card_${i + 1}`)); 
+const big = "calc(100% / 2)", small = "calc(100% / 10)";
+
+cards.forEach((card, j) => card.addEventListener("mouseover", () => {
+    cards.forEach((card, i) => {
+        if (i !== j) {
+            card.style.width = small;
+            return;
+        }
+
+        card.style.width = big;
+        document.getElementById("card_title").innerText = card.dataset.title;
+    });
+}));
