74460855fdb3cf542fe8b2f0f530b23a24de8a1a
Brian Lee <brianlee1597@gmail.com>
2022-06-12 01:32:45 +0900

    work container

--- index.js
+++ index.js
@@ -84,23 +84,23 @@ resume.addEventListener("mouseover", () => {
 const cursor = document.querySelector('.cursor');
 const cursorinner = document.querySelector('.cursor2');
 
-document.addEventListener('mousemove', function(e){
+document.addEventListener('mousemove', (e) => {
   cursor.style.transform = `translate3d(calc(${e.clientX}px - 50%), calc(${e.clientY}px - 50%), 0)`
 });
 
-document.addEventListener('mousemove', function(e){
+document.addEventListener('mousemove', (e) => {
   const x = e.clientX;
   const y = e.clientY;
   cursorinner.style.left = x + 'px';
   cursorinner.style.top = y + 'px';
 });
 
-document.addEventListener('mousedown', function(){
+document.addEventListener('mousedown', () => {
   cursor.classList.add('click');
   cursorinner.classList.add('cursorinnerhover')
 });
 
-document.addEventListener('mouseup', function(){
+document.addEventListener('mouseup', () => {
   cursor.classList.remove('click')
   cursorinner.classList.remove('cursorinnerhover')
 });
