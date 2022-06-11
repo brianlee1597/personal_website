cb6cb5a41dd5c05b70182cda4309657498061d82
Brian Lee <brianlee1597@gmail.com>
2022-06-11 17:58:39 +0900

    canvas index

--- scripts/index.js
+++ scripts/index.js
@@ -64,8 +64,6 @@ const cursor = document.querySelector('.cursor');
 const cursorinner = document.querySelector('.cursor2');
 
 document.addEventListener('mousemove', function(e){
-  const x = e.clientX;
-  const y = e.clientY;
   cursor.style.transform = `translate3d(calc(${e.clientX}px - 50%), calc(${e.clientY}px - 50%), 0)`
 });
 
