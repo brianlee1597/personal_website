c290c2a51a4fb11baa68f9bf5dcf443c0048c79d
Brian Lee <brianlee1597@gmail.com>
2022-06-26 11:36:26 -0400

    population on complete

--- contact/index.js
+++ contact/index.js
@@ -2,14 +2,6 @@ const quill = new Quill('#editor', {
     theme: 'snow'
 });
 
-function validateEmail (email) {
-    return String(email)
-    .toLowerCase()
-    .match(
-      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-    );
-};
-
 async function post (url = '', data = {}) {
     const res = await fetch(url, {
       method: 'POST',
@@ -21,25 +13,52 @@ async function post (url = '', data = {}) {
     return res.json();
 }
 
+function validateEmail (email) {
+    return String(email)
+    .toLowerCase()
+    .match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+};
+
+function populateComplete () {
+    const container = document.getElementById("container");
+    container.innerHTML = "Success";
+}
+
 async function submit () {
     const from = document.getElementById("email_input").value;
+    const title = document.getElementById("title_input").value;
     const text = quill.root.innerHTML;
+    
     if (from === "") {
         alert("Please input your email address");
         return;
-    } else if (!validateEmail(from)) {
+    } 
+
+    if (!validateEmail(from)) {
         alert("Please input a correct email address");
         return;
-    } else if (text === "<p><br></p>") {
+    }
+    
+    if (text === "<p><br></p>") {
         alert("Please populate the text field");
         return;
     }
+
+    let url = "https://contactemailsend.herokuapp.com/send_email", res;
+
     try {
-        const url = "https://contactemailsend.herokuapp.com/send_email";
-        
-        const res = await post(url, { from, text });
-        console.log(res);
-    } catch (err) {
-        console.log(err);
+        res = await post(url, { from, title, text });
+    } catch (e) {
+        alert(`There has been an error in processing your email request (${e}), please try again`);
+        return;
     }
+
+    if(res.includes("Unauthorized")) {
+        alert(`You are unauthorized to send an email from this URL`);
+        return;
+    }
+
+    populateComplete();
 }
