83b726693ce1a4ddbf8d349636ada5086a3023f7
Brian Lee <brianlee1597@gmail.com>
2022-06-26 21:17:41 -0400

    class refactor

--- contact/index.js
+++ contact/index.js
@@ -1,92 +1,95 @@
-const container = select("#container");
-const home = select("#home");
-const from = select("#email_input");
-const title = select("#title_input");
-const warning = select("#warning");
-
 const quill = new Quill('#editor', {
     theme: 'snow'
 });
 
-async function post (url = '', data = {}) {
-    const res = await fetch(url, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(data),
-    });
-    return res.json();
-}
+class Form {
+    constructor () {
+        this.container = select("#container");
+        this.home = select("#home");
+        this.from = select("#email_input");
+        this.title = select("#title_input");
+        this.warning = select("#warning");
+        this.url = "https://contactemailsend.herokuapp.com/send_email";
 
-function validateEmail (email) {
-    return String(email)
-    .toLowerCase()
-    .match(
-      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-    );
-};
+        quill.root.addEventListener("keydown", () => {
+            const text_no_space = quill.root.innerHTML.replaceAll(" ", "");
+            const box_populated = text_no_space !== "<p></p>" && text_no_space !== "<p><br></p>";
+            const has_warning = this.warning.style.visibility === "visible";
+            const authorized = !this.warning.innerText.includes("unauthorized");
+        
+            if (box_populated && has_warning && authorized) 
+                this.#clearWarning();
+        })
+        
+        this.from.addEventListener("keydown", () => {
+            const has_warning = this.warning.style.visibility === "visible";
+            const no_text_warning = !this.warning.innerText.includes("text field");
+            const authorized =  !this.warning.innerText.includes("unauthorized");
+            if (this.from.value !== "" && has_warning) {
+                if (this.warning.innerText === "Please include your email address") {
+                    this.warning.innerText = "Please include a correct email address";
+                } else if (this.#validateEmail(this.from.value) && authorized && no_text_warning) {
+                    this.#clearWarning();
+                }
+            }
+        })
+    }
 
-function populateComplete () {
-    home.style.display = "none";
+    async #post (url = '', data = {}) {
+        const res = await fetch(url, {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data),
+          });
+          return res.json();
+    }
 
-    const temp = document.getElementsByTagName("template")[0];
-    const clone = temp.content.cloneNode(true);
-    container.replaceChildren(clone);
-}
+    #validateEmail (email) {
+        return String(email)
+        .toLowerCase()
+        .match(
+          /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        );
+    };
 
-function setWarning (text) {
-    (warning.style.visibility = "visible") && (warning.innerText = text);
-}
+    #populateComplete () {
+        this.home.style.display = "none";
+    
+        const temp = document.getElementsByTagName("template")[0];
+        const clone = temp.content.cloneNode(true);
+        this.container.replaceChildren(clone);
+    }
 
-function clearWarning () {
-    (warning.style.visibility = "hidden") && (warning.innerText = "");
-}
+    #setWarning (text) {
+        this.warning.style.visibility = "visible";
+        this.warning.innerText = text;
+    }
 
-async function submit () {
-    if (warning.innerText.includes("unauthorized")) return;
+    #clearWarning () {
+        this.warning.style.visibility = "hidden";
+        this.warning.innerText = "";
+    }
 
-    const text = quill.root.innerHTML;
+    async submit () {
+        if (this.warning.innerText.includes("unauthorized")) return;
 
-    if (from.value === "") {
-        setWarning("Please include your email address");
-    } else if (!validateEmail(from.value)) {
-        setWarning("Please include a correct email address");
-    } else if (text === "<p><br></p>") {
-        setWarning("Please let me know what the email is for in the text field")
-    } else {
-        clearWarning();
-        let url = "https://contactemailsend.herokuapp.com/send_email", res;
-
-        try {
-            res = await post(url, { from, title, text });
+        const text = quill.root.innerHTML;
+    
+        if (this.from.value === "") {
+            this.#setWarning("Please include your email address");
+        } else if (!this.#validateEmail(this.from.value)) {
+            this.#setWarning("Please include a correct email address");
+        } else if (text === "<p><br></p>") {
+            this.#setWarning("Please let me know what the email is for in the text field")
+        } else {
+            this.#clearWarning();
+            const res = await this.#post(this.url, { from: this.from, title: this.title, text });
 
             if(res.includes("Unauthorized")) 
-                setWarning("You are unauthorized to send an email from this URL");
-            else populateComplete();
-        } catch (e) {
-            alert(`There has been an error in processing your email request, please try again`);
+                this.#setWarning("You are unauthorized to send an email from this URL");
+            else this.#populateComplete();
         }
     }
-}
-
-quill.root.addEventListener("keydown", () => {
-    const text_no_space = quill.root.innerHTML.replaceAll(" ", "");
-    const box_populated = text_no_space !== "<p></p>" && text_no_space !== "<p><br></p>";
-    const has_warning = warning.style.visibility === "visible";
-    const authorized = !warning.innerText.includes("unauthorized");
-
-    if (box_populated && has_warning && authorized) clearWarning();
-})
-
-from.addEventListener("keydown", () => {
-    const has_warning = warning.style.visibility === "visible";
-    const authorized =  !warning.innerText.includes("unauthorized");
-    if (from.value !== "" && has_warning) {
-        if (warning.innerText === "Please include your email address") {
-            warning.innerText = "Please include a correct email address";
-        } else if (validateEmail(from.value) && authorized) {
-            clearWarning();
-        }
-    }
-})
+};
